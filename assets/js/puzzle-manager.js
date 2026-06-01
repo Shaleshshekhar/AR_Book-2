d1ace5890f5237fdd15043f9b7b2c11b012fa395
shaleshshekhar <codershalesh@gmail.com>
2026-06-01 23:37:28 +0530

    rotation knob locker

--- assets/js/puzzle-manager.js
+++ assets/js/puzzle-manager.js
@@ -147,7 +147,7 @@ window.PuzzleManager = {
       knob.targetRotation =
         knob.currentRotation
 
-      knob.mesh.rotation.z =
+      knob.mesh.rotation.x =
         knob.currentRotation
 
       console.log(
@@ -267,7 +267,7 @@ window.PuzzleManager = {
             eased
           )
 
-        knob.mesh.rotation.z =
+        knob.mesh.rotation.x =
           rotation
 
         knob.currentRotation =
@@ -284,7 +284,7 @@ window.PuzzleManager = {
           knob.currentRotation =
             target
 
-          knob.mesh.rotation.z =
+          knob.mesh.rotation.x =
             target
 
           this.processQueue(
